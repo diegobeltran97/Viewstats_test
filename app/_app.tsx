1a70c28d7d7b6dc6582109bdc622bbfdb9b693c6
diegobeltran97 <diegosastoque97@gmail.com>
2024-02-06 12:44:33 -0500

    update style center

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,13 +1,15 @@
 // Import the AppProps type from next/app
-import type { AppProps } from 'next/app';
-import  RootLayout from './layout'
-import './globals.css'; // Adjust this path to your global CSS file location
+import type { AppProps } from "next/app";
+import RootLayout from "./layout";
+import "./globals.css"; // Adjust this path to your global CSS file location
 
 // Use the AppProps type to type the props of MyApp
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return  (<RootLayout>
-            <Component {...pageProps} />
-          </RootLayout> );
+  return (
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
 };
 
 export default MyApp;
